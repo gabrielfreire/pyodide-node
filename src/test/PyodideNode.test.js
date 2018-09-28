1afaba6815076adf061989bab6805b21efbc6da0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-28 14:30:39 +0000

    Deploy gabrielfreire/pyodide-node to github.com/gabrielfreire/pyodide-node.git:gh-pages

--- src/test/PyodideNode.test.js
+++ src/test/PyodideNode.test.js
@@ -10,6 +10,7 @@ describe('Python', function() {
             expect(pyodideNode.getModule()).not.to.equal(null);
        
         });
+
         it('should define a python function and return a result', async function() {
             this.timeout(10000);
             var pyodide = pyodideNode.getModule();
@@ -25,6 +26,45 @@ describe('Python', function() {
             expect(result).not.to.be(undefined);
             expect(result).to.be(expected);
         });
+
+        it('1. should run a python code and return the correct result', async function() {
+            this.timeout(10000);
+            var pyodide = pyodideNode.getModule();
+            var result = pyodide.runPython(
+                '[1, 2, 3]'
+            )
+            var expected = [1, 2, 3];
+            expect(result).not.to.be(undefined);
+            expect(result.length).to.be(3);
+            expect(result.length).to.be(expected.length);
+            expect(result[0]).to.be(expected[0]);
+            expect(result[1]).to.be(expected[1]);
+            expect(result[2]).to.be(expected[2]);
+        });
+
+        it('2. should run a python code and return the correct result', async function() {
+            this.timeout(10000);
+            var pyodide = pyodideNode.getModule();
+            var result = pyodide.runPython(
+                '{42: 64}'
+            )
+            expect(result).not.to.be(undefined);
+            expect(typeof result).to.equal("object");
+            expect(result[42]).to.equal(64);
+        });
+
+        it('3. should run a python code and return the correct result', async function() {
+            this.timeout(10000);
+            var pyodide = pyodideNode.getModule();
+            var result = pyodide.runPython(
+                "b\'bytes\'"
+            )
+            expect(result).not.to.be(undefined);
+            expect(result instanceof Uint8ClampedArray).to.be(true);
+            expect(result.length).to.be(5);
+            expect(result[0]).to.equal(98);
+        });
+
         it('should import numpy and perform operations', async function() {
             this.timeout(20000);
             var pyodide = pyodideNode.getModule();
@@ -39,14 +79,17 @@ describe('Python', function() {
                 '   return c.tolist()');
             const test = pyodide.pyimport('test');
             const result = test();
+            const _result = new Array(...result);
             const expected = new Array(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 1, 2, 3);
 
             expect(result).not.to.be(undefined);
             expect(result.constructor.name).to.equal("Array");
+            expect(result.length).to.be(expected.length);
             expect(result[0]).to.equal(expected[0]);
             expect(result[1]).to.equal(expected[1]);
             expect(result[3]).to.equal(expected[3]);
             expect(result[result.length-1]).to.equal(expected[expected.length-1]);
         });
+        
     });
 });
